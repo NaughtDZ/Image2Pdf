108e0fcb6d9255a43433c25ecc87ae4765c5ed4c
NaughtDZ <37768911+NaughtDZ@users.noreply.github.com>
2020-12-21 20:22:40 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Conver = New System.Windows.Forms.Button()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(93, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(232, 21)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(93, 39)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(232, 21)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(4, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(73, 20)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "图片文件夹"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(4, 39)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(73, 21)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "输出文件"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Conver
+        '
+        Me.Conver.Location = New System.Drawing.Point(4, 66)
+        Me.Conver.Name = "Conver"
+        Me.Conver.Size = New System.Drawing.Size(73, 32)
+        Me.Conver.TabIndex = 4
+        Me.Conver.Text = "转换"
+        Me.Conver.UseVisualStyleBackColor = True
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Enabled = False
+        Me.TrackBar1.Location = New System.Drawing.Point(181, 72)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(144, 45)
+        Me.TrackBar1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(91, 75)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 12)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "默认DPI"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(144, 72)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(31, 21)
+        Me.TextBox3.TabIndex = 7
+        Me.TextBox3.Text = "72"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(335, 111)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Conver)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Muti IMG2PDF"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Conver As Button
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.IO '虽然写在这里，但是pdfsharp和sytem里都有Drawing等字类同名，请下面详写
+Imports PdfSharp
+Imports PdfSharp.Pdf
+Imports PdfSharp.Drawing
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FolderBrowserDialog1.ShowDialog() '读图片文件夹
+        TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        TrackBar1.Value = 0
+    End Sub
+
+    Dim filelist() As String
+    Private Function GetAllImage(ByVal imgfold As String)
+        Dim allfile() As String
+        Try
+            allfile = System.IO.Directory.GetFiles(imgfold)
+        Catch e As Exception
+            MsgBox(e.ToString)
+            Exit Function
+        End Try
+        Dim imgfile As Collection = New Collection
+        For Each things In allfile
+            If things.ToLower Like "*.jpg" OrElse things.ToLower Like "*.pmg" OrElse things.ToLower Like "*.jpeg" OrElse things.ToLower Like "*.bmp" Then
+                'Debug.WriteLine("筛选之后：" & things)
+                imgfile.Add(things)
+            End If
+        Next
+        Return imgfile
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SaveFileDialog1.ShowDialog()
+        TextBox2.Text = SaveFileDialog1.FileName
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Conver.Click
+        Conver.Enabled = False
+        TrackBar1.Value = 0
+        Dim w8t2con = GetAllImage(TextBox1.Text)
+        If w8t2con.Count = 0 Then
+            MsgBox("没有找到图片")
+            Conver.Enabled = True
+            Exit Sub
+        End If '直接有问题直接退出，不做分支了
+        Merg2pdf(w8t2con)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SaveFileDialog1.AddExtension = True
+        SaveFileDialog1.Filter = "PDF文件(*.pdf)|*.pdf"
+    End Sub
+
+    Private Sub Merg2pdf(ByVal w8t2con)
+        TrackBar1.Maximum = w8t2con.Count
+        Dim document As PdfDocument = New PdfDocument '创建pdf文件
+        For Each img In w8t2con
+            Dim page As PdfPage = document.AddPage() '创建新页
+            Dim gfx As XGraphics = XGraphics.FromPdfPage(page) '创建画布在page上
+            Dim ximg As XImage = XImage.FromFile(img) '创建gfx可用的image
+            Dim g As Graphics
+            g = Graphics.FromImage(Image.FromFile(img))
+            Debug.WriteLine(ximg.HorizontalResolution) 'pdf打印页面大小与DPI有关
+            page.Width = ximg.PixelWidth '设置页面为图片分辨率,piexel是分辨率，width是通过dpi转换后的大小
+            page.Height = ximg.PixelHeight
+            Debug.WriteLine("{0} x:{1} y:{2}", img, page.Width, page.Height)
+            Debug.WriteLine("x:{0} y:{1}", g.DpiX / TextBox3.Text, g.DpiY / TextBox3.Text)
+            gfx.ScaleTransform(ximg.HorizontalResolution / TextBox3.Text)
+            gfx.DrawImage(ximg, 0, 0)
+            TrackBar1.Value += 1
+        Next
+        document.Save(TextBox2.Text)
+        Beep()
+        document.Close()
+        Conver.Enabled = True
+    End Sub
+
+End Class
